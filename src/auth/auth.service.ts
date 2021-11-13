8b5518e7134e26ff58971606b9d3c6390e43609d
Mukut Brahma <mukutb.mac@gmail.com>
2021-11-13 16:16:34 +0530

    signin: returning accessToken

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { ConflictException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
+import { JwtPayload } from './auth.types';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
 
@@ -8,6 +10,7 @@ export class AuthService {
     constructor(
         @InjectRepository(UsersRepository)
         private readonly userRepository: UsersRepository,
+        private readonly jwtService: JwtService
     ) { }
 
     async signUp(authCredentialDto: AuthCredentialsDto) {
@@ -24,9 +27,11 @@ export class AuthService {
 
     async signIn(authCredentialDto: AuthCredentialsDto) {
         const email = await this.userRepository.validateUserPassword(authCredentialDto);
+        const payload: JwtPayload = { email };
+        const accessToken = this.jwtService.sign(payload);
         if (!email) {
             throw new UnauthorizedException('Invalid credentials');
         }
-        return { email };
+        return { accessToken };
     }
 }
